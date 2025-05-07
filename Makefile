815a23731d77850f0abf0658f4ba23f402b970bd
Roman2 <rom.kawirshin@gmail.com>
2025-05-07 23:02:27 +0300

    Init project

---
+++ Makefile
@@ -0,0 +1,10 @@
+build:
+	make -C app build
+run-dist:
+	make -C app run-dist
+checkstyle:
+	make -C app checkstyle
+test:
+	make -C app test
+report:
+	make -C app report
---
+++ app/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("java")
+    application
+    checkstyle
+    jacoco
+    id("org.sonarqube") version "6.0.1.5171"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+sonar {
+    properties {
+        property("sonar.projectKey", "rom-kavyrshin_java-project-78")
+        property("sonar.organization", "rom-kavyrshin")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required = true
+    }
+}
