a0f92826d10bc17fcb982b4a3e9501321c0272d3
Roman2 <rom.kawirshin@gmail.com>
2025-05-08 21:14:14 +0300

    Добавлен пример использования в App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,16 @@
 package hexlet.code;
 
+import hexlet.code.schemas.StringSchema;
+
 public class App {
 
     public static void main(String[] args) {
+        Validator validator = new Validator();
+        StringSchema schema = validator.string();
 
+        schema.required().minLength(5).contains("some");
+
+        System.out.println("schema.isValid(\"something\"): " + schema.isValid("something"));
+        System.out.println("schema.isValid(\"thing\"): " + schema.isValid("thing"));
     }
 }
