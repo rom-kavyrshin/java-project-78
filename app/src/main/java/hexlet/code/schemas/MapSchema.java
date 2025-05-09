f21c5d2273d41374eeb3720c2eab15a68efb8b75
Roman2 <rom.kawirshin@gmail.com>
2025-05-09 21:23:06 +0300

    Реализован MapSchema

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -1,19 +1,31 @@
 package hexlet.code.schemas;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.function.BiPredicate;
 
 public class MapSchema extends BaseSchema<Map<String, String>> {
 
+    private final BiPredicate<Integer, Map<String, String>> notNullTest = (ignore, test) -> test != null;
+    private final BiPredicate<Integer, Map<String, String>> sizeofTest = (exactSize, test) -> test.size() == exactSize;
+
+    private final LinkedHashMap<String, ValidationProperty<?, Map<String, String>>> linkedHashMap =
+            new LinkedHashMap<>();
+
     public MapSchema required() {
+        linkedHashMap.putFirst("required", new ValidationProperty<>(1, notNullTest));
         return this;
     }
 
     public MapSchema sizeof(int size) {
+        linkedHashMap.put("sizeof", new ValidationProperty<>(size, sizeofTest));
         return this;
     }
 
     @Override
     public boolean isValid(Map<String, String> stringStringMap) {
-        return false;
+        return linkedHashMap.values()
+                .stream()
+                .allMatch(it -> it.test(stringStringMap));
     }
 }
