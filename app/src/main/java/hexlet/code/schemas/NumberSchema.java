076ad133ef0b2f5bead4e607cc216e074ae2eb13
Roman2 <rom.kawirshin@gmail.com>
2025-05-09 17:49:11 +0300

    Реализован класс NumberSchema

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -1,21 +1,38 @@
 package hexlet.code.schemas;
 
+import hexlet.code.Pair;
+
+import java.util.LinkedHashMap;
+import java.util.function.BiPredicate;
+
 public class NumberSchema extends BaseSchema<Integer> {
 
+    private final BiPredicate<Integer, Integer> notNullTest = (ignore, test) -> test != null;
+    private final BiPredicate<Integer, Integer> positiveTest = (minLength, test) -> test > 0;
+    private final BiPredicate<Pair<Integer, Integer>, Integer> inRangeTest =
+            (contains, test) -> contains.first() <= test && test <= contains.second();
+
+    private final LinkedHashMap<String, ValidationProperty<?, Integer>> linkedHashMap = new LinkedHashMap<>();
+
     public NumberSchema required() {
+        linkedHashMap.putFirst("required", new ValidationProperty<>(1, notNullTest));
         return this;
     }
 
     public NumberSchema positive() {
+        linkedHashMap.put("positive", new ValidationProperty<>(1, positiveTest));
         return this;
     }
 
     public NumberSchema range(int start, int end) {
+        linkedHashMap.put("range", new ValidationProperty<>(Pair.of(start, end), inRangeTest));
         return this;
     }
 
     @Override
     public boolean isValid(Integer number) {
-        return false;
+        return linkedHashMap.values()
+                .stream()
+                .allMatch((it) -> it.test(number));
     }
 }
