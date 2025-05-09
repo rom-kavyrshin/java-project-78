6a317e485fb35df96c8fc90675cd2e0969c80a60
Roman2 <rom.kawirshin@gmail.com>
2025-05-09 18:05:01 +0300

    Добавлен пример использования для numberSchema

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,16 +1,19 @@
 package hexlet.code;
 
+import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
 
 public class App {
 
     public static void main(String[] args) {
         Validator validator = new Validator();
-        StringSchema schema = validator.string();
+        StringSchema stringSchema = validator.string();
+        NumberSchema numberSchema = validator.number();
 
-        schema.required().minLength(5).contains("some");
+        stringSchema.required().minLength(5).contains("some");
+        numberSchema.required().positive().range(5, 10);
 
-        System.out.println("schema.isValid(\"something\"): " + schema.isValid("something"));
-        System.out.println("schema.isValid(\"thing\"): " + schema.isValid("thing"));
+        System.out.println("stringSchema.isValid(\"something\"): " + stringSchema.isValid("something"));
+        System.out.println("numberSchema.isValid(7): " + numberSchema.isValid(7));
     }
 }
