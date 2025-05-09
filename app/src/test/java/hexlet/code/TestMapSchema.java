253f9e21ada49c610194984859f7e9fe673e070f
Roman2 <rom.kawirshin@gmail.com>
2025-05-09 22:38:58 +0300

    Реализованы тесты для MapSchema shape

--- app/src/test/java/hexlet/code/TestMapSchema.java
+++ app/src/test/java/hexlet/code/TestMapSchema.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import hexlet.code.schemas.BaseSchema;
 import hexlet.code.schemas.MapSchema;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,11 +12,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TestMapSchema {
 
+    Validator validator;
     MapSchema mapSchema;
 
     @BeforeEach
     void beforeEach() {
-        mapSchema = new Validator().map();
+        validator = new Validator();
+        mapSchema = validator.map();
     }
 
     @Test
@@ -51,6 +54,64 @@ class TestMapSchema {
 
         testMap.put("thirdKey", "thirdValue");
         assertFalse(mapSchema.isValid(testMap));
+    }
+
+    @Test
+    void testShapeWrongKeys() {
+        var schemas = new HashMap<String, BaseSchema<?>>();
+        var testMap = new HashMap<String, Object>();
+
+        assertTrue(mapSchema.isValid(testMap));
+
+        schemas.put("firstName", validator.string().required());
+        schemas.put("lastName", validator.string().required().minLength(2));
+
+        mapSchema.required().sizeof(2).shape(schemas);
+
+        assertFalse(mapSchema.isValid(testMap));
+        testMap.put("firstKey", "firstValue");
+        testMap.put("secondKey", "secondValue");
+        assertFalse(mapSchema.isValid(testMap));
+
+        testMap.clear();
+
+        assertFalse(mapSchema.isValid(testMap));
+        testMap.put("firstName", "John");
+        testMap.put("lastName", "Lenon");
+
+        assertTrue(mapSchema.isValid(testMap));
+    }
+
+    @Test
+    void testShapeExtended() {
+        var schemas = new HashMap<String, BaseSchema<?>>();
+        var testMap = new HashMap<String, Object>();
+
+        assertTrue(mapSchema.isValid(testMap));
+
+        schemas.put("firstName", validator.string().required());
+        schemas.put("lastName", validator.string().required().minLength(2));
+        schemas.put("age", validator.number().required().range(21, 30));
+
+        mapSchema.required().sizeof(3).shape(schemas);
+
+        testMap.put("firstName", null);
+        testMap.put("lastName", "Lenon");
+        testMap.put("age", 25);
+        assertFalse(mapSchema.isValid(testMap));
+
+        testMap.put("firstName", "John");
+        assertTrue(mapSchema.isValid(testMap));
+
+        testMap.put("lastName", "L");
+        assertFalse(mapSchema.isValid(testMap));
+        testMap.put("lastName", "Lenon");
+        assertTrue(mapSchema.isValid(testMap));
+
+        testMap.put("age", 31);
+        assertFalse(mapSchema.isValid(testMap));
+        testMap.put("age", 25);
+        assertTrue(mapSchema.isValid(testMap));
     }
 
     @Test
